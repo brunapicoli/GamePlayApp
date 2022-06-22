d72e22459d0866a993aa1a283877bc1b030b011c
Bruna <brunatpicoli@outlook.com>
2022-06-22 09:02:01 -0300

    criação do hook useAuth

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
-import {Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
 import { Routes } from './src/routes';
 import { StatusBar } from 'react-native';
 import { Background } from './src/components/Background';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +27,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </Background>
   );
 }
